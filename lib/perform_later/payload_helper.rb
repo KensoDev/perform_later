8eacee530e50ed2c47f45dad9908c295effdce93
Avi Tzurel <avi@kensodev.com>
2012-05-22 23:00:23 +0300

    Changed it to be flattened in the payload helper as well

--- lib/perform_later/payload_helper.rb
+++ lib/perform_later/payload_helper.rb
@@ -1,8 +1,8 @@
 module PerformLater
   class PayloadHelper
-    def self.get_digest(klass, method, args)
+    def self.get_digest(klass, method, *args)
       puts [klass, method, args]
-      digest = Digest::MD5.hexdigest({:class => klass, :method => method, :args => args}.to_s)
+      digest = Digest::MD5.hexdigest({:class => klass, :method => method, :args => args.flatten}.to_s)
       
       puts "Digest is: #{digest}"
 
