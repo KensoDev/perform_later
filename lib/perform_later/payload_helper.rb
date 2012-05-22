69a7b63cd36392c1296f1256e209be3a2444f60b
Avi Tzurel <avi@kensodev.com>
2012-05-23 00:35:51 +0300

    Convert the method into a string at all times

--- lib/perform_later/payload_helper.rb
+++ lib/perform_later/payload_helper.rb
@@ -2,9 +2,11 @@ module PerformLater
   class PayloadHelper
     def self.get_digest(klass, method, *args)
       args = args.flatten
+      payload = {:class => klass, :method => method.to_s, :args => args}.to_s
       
-      digest = Digest::MD5.hexdigest({:class => klass, :method => method, :args => args}.to_s)
-      puts [{ :class => klass, :method => method, :args => args }.to_s]
+      puts payload
+      digest = Digest::MD5.hexdigest(payload)
+      
 
       puts "Digest is: #{digest}"
       digest
