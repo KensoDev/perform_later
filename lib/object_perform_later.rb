e3864ed5ec6b0ecc9bd35a509e685c463bcadf95
Avi Tzurel <avi@kensodev.com>
2012-05-22 19:02:04 +0300

    Removed the old object worker

--- lib/object_perform_later.rb
+++ lib/object_perform_later.rb
@@ -1,12 +1,15 @@
 module ObjectPerformLater
+
   def perform_later(queue, method, *args)
     if PerformLater.config.enabled?
       args = PerformLater::ArgsParser.args_to_resque(args)
-      Resque::Job.create(queue, ObjectWorker, self.name, method, *args)
+      Resque::Job.create(queue, PerformLater::Workers::Objects::Worker, self.name, method, *args)
     else
       self.send(method, *args)
     end
   end
+
 end
 
-Object.send :include, ObjectPerformLater
+Object.send(:include, ObjectPerformLater)
+
--- lib/perform_later.rb
+++ lib/perform_later.rb
@@ -6,10 +6,11 @@ require 'resque-loner'
 require 'perform_later/args_parser'
 require 'active_record'
 require 'resque_mailer_patch'
-require 'object_worker'
 require 'object_perform_later'
 require 'perform_later/workers/active_record/worker'
 require 'perform_later/workers/active_record/lone_worker'
+require 'perform_later/workers/objects/worker'
+require 'perform_later/workers/objects/lone_worker'
 
 module PerformLater
   def self.config
@@ -28,5 +29,3 @@ end
 ActiveSupport.on_load(:active_record) do
   include Resque::Plugins::Later::Method
 end
-
-
