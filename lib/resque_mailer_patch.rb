e4d70d689e7879b077bb85e0e40d012b47db99db
Avi Tzurel <avi@kensodev.com>
2012-05-23 21:37:23 +0300

    fixed the mailer patch

--- lib/resque_mailer_patch.rb
+++ lib/resque_mailer_patch.rb
@@ -2,14 +2,14 @@ module Resque
   module Mailer
     module ClassMethods
       def perform(action, *args)
-        args = ResquePerformLater.args_from_resque(args)
+        args = PerformLater::ArgsParser.args_from_resque(args)
         self.send(:new, action, *args).message.deliver
       end
     end
 
     class MessageDecoy
       def deliver
-        args = ResquePerformLater.args_to_resque(@args)
+        args = PerformLater::ArgsParser.args_to_resque(@args)
         resque.enqueue(@mailer_class, @method_name, *args)
       end
     end
